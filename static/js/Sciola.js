707cd31ad7d71574734b49408e07c4b230c6fe65
Leandro Sciola <118067473+Leandro-Sciola@users.noreply.github.com>
2022-11-22 12:55:47 -0300

    Update Sciola.js

--- static/js/Sciola.js
+++ static/js/Sciola.js
@@ -1,1 +1,44 @@
+class Sciola {
 
+    /**
+     * Class constructor
+     *
+     * @access public
+     */
+    constructor() {
+        window.$_      = [];
+        window.global  = window;
+        global.type_of = obj => Object.prototype
+                                      .toString
+                                      .call(obj)
+                                      .slice(8, -1)
+                                      .toLowerCase();
+        if (!window["module"]) {
+            global.module  = {};
+            module.exports = null;
+        }
+    }
+
+    /**
+     * Initialization
+     *
+     * @access public
+     */
+    init() {
+        $_["document"]  = new JDocument();
+        $_["http"]      = new JHttp();
+        $_["file"]      = new JFile();
+        $_["language"]  = new JLanguage();
+        $_["element"]   = new JElement();
+        $_["event"]     = new JEvent();
+        $_["time"]      = new JTime();
+        $_["component"] = new JComponent();
+        $_["document"].ready(function () {
+            $_["component"].navbar();
+            $_["component"].navbar().effect("dark-to-light");
+        });
+        $_["document"].ready(function () {
+            $("[onload]").trigger("onload");
+        });
+    }
+}
