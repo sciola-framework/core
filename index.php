675489b4199fb9da7355c49ac9260f3d6ecc98ce
Leandro Sciola <118067473+Leandro-Sciola@users.noreply.github.com>
2022-12-14 12:35:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,23 +2,35 @@
 /**
  * Sciola
  *
- * @version 1.0.0
+ * @version 1.0.1
  */
 class Sciola
 {
     /**
-     * init
+     * index
      *
+     * @param string $path
      * @access public
      */
-    public static function init()
+    public static function index($path)
+    {
+        if (is_file($path . '/public' . $_SERVER['REQUEST_URI'])) return false;
+        self::autoload($path);
+    }
+
+    /**
+     * autoload
+     *
+     * @param string $path
+     * @access private
+     */
+    private static function autoload($path)
     {
         try {
             $file = dirname(__FILE__) . '/vendor/autoload.php';
             if (file_exists($file)) {
                 include_once $file;
-                $file = explode('/packages/node_modules/sciola', $file)[0];
-                return Sciola\Settings::init($file);
+                return Sciola\Settings::init($path);
             }
             throw new Exception('Error: The autoload.php file was not found!');
         } catch (Exception $e) {
