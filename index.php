db29ff14c7eb82da9a9a38deb223616ea2078dda
Leandro Sciola <118067473+Leandro-Sciola@users.noreply.github.com>
2023-01-10 15:42:19 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Sciola
  *
- * @version 1.0.3
+ * @version 1.0.4
  */
 class Sciola
 {
@@ -18,7 +18,8 @@ class Sciola
         foreach ($path as $key => $value) {
             $path[$key] = $app . $path[$key];
         }
-        $path["app"] = $app;
+        $path["app"]  = $app;
+        $path["core"] = dirname(__FILE__);
         define('PATH', $path);
         if (is_file(PATH['public'] . $_SERVER['REQUEST_URI'])) {
             return false;
@@ -34,7 +35,7 @@ class Sciola
     private static function autoload()
     {
         try {
-            $file = dirname(__FILE__) . '/vendor/autoload.php';
+            $file = PATH['core'] . '/vendor/autoload.php';
             if (file_exists($file)) {
                 include_once $file;
                 $file = PATH['vendor'] . '/autoload.php';
